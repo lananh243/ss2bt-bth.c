cc62c013da492af58d1bf840d60ebdaca384cafd
lananh243 <147390706+lananh243@users.noreply.github.com>
2023-11-08 11:54:12 +0700

    Add files via upload

---
+++ bt1 ss2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	const float PI = 3.14;
+	int canh;
+	int chieu_rong;
+	int chieu_dai;
+	int ban_kinh;
+	int dienTichHinhTron = ban_kinh * ban_kinh * PI;
+	int chuViHinhTron = (2* ban_kinh)* PI;
+	int dienTichHinhVuong = canh*canh;
+	int chuViHinhVuong = canh*4;
+	int dienTichHinhChuNhat = chieu_dai * chieu_rong;
+	int chuViHinhChuNhat  = (chieu_dai+chieu_rong)*2;
+	
+}
