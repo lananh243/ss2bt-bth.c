41a285c8a7b745f6a1af1298cfc0dfd5a204aa8b
lananh243 <147390706+lananh243@users.noreply.github.com>
2023-11-08 11:52:14 +0700

    Add files via upload

---
+++ Session02_Practic01.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h> 
+main(){
+	int age; 
+	bool sex;
+	float mark;
+	age = 18;
+	sex = true;
+	mark = 9.5;
+	char studentld[] = {'S', 'V', 'O','O','1'};
+	char studentName[] = " NGUYEN VAN A";
+	double munber1 = 6.5, number2 = 8.2, number3 = -7.4; 
+}
