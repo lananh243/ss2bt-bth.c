6a7a5f2e502abbd37f7e38f5adbdaa1e94f5cf12
lananh243 <147390706+lananh243@users.noreply.github.com>
2023-11-16 11:10:00 +0700

    Add files via upload

---
+++ ss2.1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	float PI=3.14;
+	int a,length,width,r;
+	int acreageRectangle=(length*width);
+	int perimerRectangle=(length*width)*2;
+	int acreageSquare=a*a;
+	int perimerSquare=4*a;
+	float acreageCircle=r*r*PI;
+	float perimerCircle=(r*2)*PI;
+}
